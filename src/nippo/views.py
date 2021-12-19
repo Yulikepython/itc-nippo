dc35c9ec5c1831be4adab8be3e346c348d02e978
Yu <admin@itc.tokyo>
2021-12-19 21:46:33 +0900

    [2-3] URLから値を受け取る

--- src/nippo/views.py
+++ src/nippo/views.py
@@ -4,10 +4,11 @@ from random import randint
 def nippoListView(request):
 	return render(request, "nippo/nippo-list.html")
 
-def nippoDetailView(request):
+def nippoDetailView(request, number):
     template_name="nippo/nippo-detail.html"
     random_int = randint(1,10)
     ctx = {
-    "random_number": random_int,
+        "random_number": random_int,
+        "number": number,
     }
     return render(request, template_name, ctx)
